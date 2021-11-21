33b878af437d999ceecc195ca905d370e3735349
Sander Iwase <sanderiw@gmail.com>
2021-11-20 21:59:14 -0300

    organize routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
+
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationRoutes);
-app.listen(3333, () => console.log("Server up and running"));
+app.use(router);
+
+app.listen(3333, () => {
+  return console.log("Server up and running");
+});
