69ea04df0296a25b27abb3d9eeca3b1ce0c5e9f6
Sander Iwase <sanderiw@gmail.com>
2021-11-23 19:41:29 -0300

    create import category

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,9 @@
+import { ImportCategoryController } from "./ImportCategoryController";
+
+class ImportCategoryUseCase {
+  execute(file: any) {
+    console.log(file);
+  }
+}
+
+export { ImportCategoryUseCase };
