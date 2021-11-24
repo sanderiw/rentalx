394b3bf1d80fe0537544c841c146f921812b2743
Sander Iwase <sanderiw@gmail.com>
2021-11-23 22:13:35 -0300

    adding stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,28 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File) {
-    console.log(file);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File) {
+    const stream = fs.createReadStream(file.path);
+    const categories: IImportCategory[] = [];
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {});
   }
+
+  execute(file: Express.Multer.File): void {}
 }
 
 export { ImportCategoryUseCase };
